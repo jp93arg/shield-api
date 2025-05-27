b24c8c458c7cf7d62f10e514e979e86278dceefc
jp93arg <jpcarrafancq@gmail.com>
2025-05-27 19:55:37 -0300

    add users model and migration

---
+++ src/models/user.ts
@@ -0,0 +1,33 @@
+import { Model, DataTypes } from "sequelize";
+import sequelize from "../../config/db";
+
+class User extends Model {
+  public id!: number;
+  public email!: string;
+  public password!: string;
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  },
+  {
+    tableName: "Users",
+    sequelize
+  }
+);
+
+export default User;
