e978c416ca2fb591797fd8c10fe3d202e09d2870
jp93arg <jpcarrafancq@gmail.com>
2025-05-27 19:55:12 -0300

    add db connection

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,13 @@
 import app from "./app";
+import sequelize from "../config/db";
 
 const PORT = process.env.PORT || 3000;
+
+sequelize
+  .authenticate()
+  .then(() => console.log("✅ Connected to DB"))
+  .catch((err) => console.error("❌ DB connection failed:", err));
+
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
